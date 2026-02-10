35591c9e6e410a2ffec509b42525ac80e35cc44d
kate <34276633+mechanikate@users.noreply.github.com>
2026-02-09 19:17:40 -0500

    1.3.0 - add timer in paint menu

--- wplace-charges-time.user.js
+++ wplace-charges-time.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         max charges time
 // @namespace    https://github.com/mechanikate/wplace-charges-time
-// @version      1.2.3
+// @version      1.3.0
 // @description  adds a timer counting down to when you will have max charges above the Paint button for wplace
 // @license      MIT
 // @author       mechanikate
@@ -60,40 +60,44 @@ unsafeWindow.updateChargeData=()=>{
         console.log("(max charges time) Charge data successfully updated");
     });
 };
-window.setInterval(() => { // just an interval because I don't feel like making this more complex
-    let plainChargeNode = document.querySelector(".btn.btn-primary.btn-lg.relative.z-30>.flex.items-center>.flex.items-center>span>.w-7.text-xs");
+setInterval(() => { // just an interval because I don't feel like making this more complex
+    let plainChargeNode = document.querySelector(".btn.btn-primary.btn-lg>.flex.items-center>.flex.items-center>span>.w-7.text-xs");
     let dropletShopNodes = document.querySelectorAll(".btn.btn-xl.btn-primary.relative.mt-3.h-10");
     if(valueMissing(dropletShopNodes, true) && !updateQueued) {
         try { [0,1].forEach(i => dropletShopNodes[i].addEventListener("click", ()=>setTimeout(unsafeWindow.updateChargeData, 3500))); } catch {} // "+5 Max. Charges" and "+30 Charges" buttons;
     }
     if(valueMissing(plainChargeNode)) { // buttons missing/null? if so, add the event listeners in 3500ms:
-        try { document.querySelector(".absolute.bottom-0 > .btn.btn-primary").addEventListener("click", ()=>setTimeout(unsafeWindow.updateChargeData, 3500)); } catch {} // the timeout is a really bad solution to waiting for loading. wtv
-        return;
+        try { document.querySelector(".absolute.bottom-0 > .btn.btn-primary").addEventListener("click", ()=>setTimeout(unsafeWindow.updateChargeData, 3500)); return; } catch {} // the timeout is a really bad solution to waiting for loading. wtv
     }
     let plainChargeHTML = plainChargeNode.innerHTML;
     let secondsLeftForCharge = replaceNaN(parseInt(plainChargeHTML.match(/0:([0-9]+)/)[1]), 0); // get the # of seconds left (0-30) until we get 1 more charge
     let remainingSeconds = (maxCharges-charges)*30+secondsLeftForCharge;
     let maxSeconds = maxCharges*30;
-    let existingEle = document.getElementById("timeTillMaxCharges");
+    let existingEles = [document.getElementById("timeTillMaxCharges"), document.getElementById("timeTillMaxChargesOpen")];
     let toMaxStr = `(${toHHMMSS(remainingSeconds)}${showMax ? "/"+toHHMMSS(maxSeconds) : ""} to max)`;
-    if(typeof(existingEle) != "undefined" && existingEle != null) {
+    // if ele doesn't exist, set it up:
+    let newSpans = [document.createElement("p"), document.createElement("p")];
+    newSpans.forEach((newSpan,i) => {
+        newSpan.classList.add("w-7", "text-xs");
+        newSpan.innerHTML = toMaxStr;
+        newSpan.id = ["timeTillMaxCharges","timeTillMaxChargesOpen"][i];
+        newSpan.style.width = "100%";
+        newSpan.style.paddingBottom = "10px";
+        newSpan.style.textAlign = "center";
+         if(chargesFullColoring) newSpan.style.color = determineColor(remainingSeconds/maxCharges/30);
+    });
+    const parentNodes = [document.querySelector(".btn.btn-primary.btn-lg.relative.z-30"), document.querySelector(".absolute.bottom-0.left-0.z-50.w-full > .rounded-t-box.bg-base-100.border-base-300.w-full.border-t.py-3 > .relative.px-3 > .mt-3.mb-4")];
+    existingEles.forEach((existingEle,i) => {
+        console.log(existingEle);
+        if(typeof(existingEle) == "undefined" || existingEle == null) {
+            if(parentNodes[i] === null) return;
+            return parentNodes[i].parentElement.insertBefore(newSpans[i], parentNodes[i]);
+        }
         existingEle.innerHTML = toMaxStr; // handle if our element alr exists
         if(chargesFullColoring) existingEle.style.color = determineColor(remainingSeconds/maxCharges/30);
-        return;
-    }
-    // if ele doesn't exist, set it up:
-    let newSpan = document.createElement("p");
-    newSpan.classList.add("w-7", "text-xs");
-    newSpan.innerHTML = toMaxStr;
-    newSpan.id = "timeTillMaxCharges";
-    newSpan.style.width = "100%";
-    newSpan.style.paddingBottom = "10px";
-    newSpan.style.textAlign = "center";
-    if(chargesFullColoring) newSpan.style.color = determineColor(remainingSeconds/maxCharges/30);
-    const btnNode = document.querySelector(".btn.btn-primary.btn-lg.relative.z-30");
-    document.querySelector(".btn.btn-primary.btn-lg.relative.z-30").parentElement.insertBefore(newSpan, btnNode);
-    document.querySelector(".btn.btn-primary.btn-lg.relative.z-30").parentElement.addEventListener("click", unsafeWindow.updateChargeData); // "Paint" button
-}, 750);
+        return false;
+    });
+}, 75);
 window.setInterval(unsafeWindow.updateChargeData, 600000); // just to be safe, update charge data every 10 mins too
 unsafeWindow.updateChargeData();
 updateToggles();
